9531cd373a6f5e7cf1adfdc360cc05878c82a7d6
Rizwan Hameed <rizwanbutt314@gmail.com>
2018-01-27 17:09:19 +0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,18 @@ jQuery(document).ready(function() {
     var activeCameraId = null;
     var cameras = [];
     var scans = [];
+    var selected_data = {
+        'q1_opts': '',
+        'q2_opts': '',
+        'q3_opts': '',
+    };
+
+
+    $('input[type="radio"]').on('click', function() {
+        var option_name = $(this).attr('name');
+        var option_value = $(this).attr('value');
+        selected_data[option_name] = option_value;
+    });
 
 
     /*
@@ -19,9 +31,6 @@ jQuery(document).ready(function() {
     $('#top-navbar-1').on('hidden.bs.collapse', function(){
     	$.backstretch("resize");
     });
-
-
-
     
     /*
         Form
@@ -47,6 +56,11 @@ jQuery(document).ready(function() {
         var button_id = $(this).attr("id");
 
         if(button_id == "finish_survey"){
+            selected_data = {
+                'q1_opts': '',
+                'q2_opts': '',
+                'q3_opts': '',
+            };
             parent_fieldset.fadeOut(400, function() {
                 $(this).next().fadeIn();
             });
@@ -54,9 +68,11 @@ jQuery(document).ready(function() {
             $('#btn_start').css('display','');
         }
         else if(button_id == "btn_continue"){
+            $("#youtube_video")[0].src = $("#youtube_video")[0].src.replace('?autoplay=1', '');
             parent_fieldset.fadeOut(400, function() {
                 $(this).next().fadeIn();
             });
+
         }
         else{
             var selected_option = parent_fieldset.find("input[type='radio']:checked").val();
@@ -101,15 +117,8 @@ jQuery(document).ready(function() {
 
         scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
         scanner.addListener('scan', function (content) {
-            console.log(content);
             var parsed_content = parse_qr_code(content);
-            //login(parsed_content);
-
-            $('fieldset[style*="display: block"]').fadeOut(400, function() {
-                $('.registration-form fieldset:nth-child(2)').fadeIn('slow');
-                $("#youtube_video")[0].src += "?autoplay=1";
-            });
-
+            login(parsed_content);
         });
         Instascan.Camera.getCameras().then(function (cameras) {
             if (cameras.length > 0) {
@@ -135,22 +144,25 @@ jQuery(document).ready(function() {
 
         $.ajax({
             type: "POST",
-            url: "/qr/php/main.php",
+            url: "/qr-survey-app/php/main.php",
             dataType: 'json',
             async: false,
             data: qr_json,
             success: function (response){
-                if(!response['success']){
+                if(response['success'] == true){
                     $('.qr_error').text("");
-                    //window.location.href = "/qr/dashboard.php";
+                    scanner.stop();
+                    $('fieldset[style*="display: block"]').fadeOut(400, function() {
+                        $('.registration-form fieldset:nth-child(2)').fadeIn('slow');
+                        $("#youtube_video")[0].src += "?autoplay=1";
+                    });
                 }
                 else{
-                    $('.qr_error').text("Invalid QR");
+                    $('.qr_error').text("Invalid QR2");
                 }
             },
             error: function (e){
-                console.log("Error ",e);
-                $('.qr_error').text("Invalid QR");
+                $('.qr_error').text("Invalid QR3");
             }
         });
     }
